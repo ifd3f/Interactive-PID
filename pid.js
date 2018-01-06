81f151f2fa42233d780deed842a6d2cb784cb9db
Astrid Augusta Yu <astridyu3.14@gmail.com>
2018-01-05 19:55:09 -0800

    Make those params actually work

--- pid.js
+++ pid.js
@@ -1,15 +1,14 @@
 const LABEL_COLOR = "rgb(255,255,255)";
 const GRID_COLOR = "rgb(128,128,128)";
+const EPSILON = 0.00000001;
 
-var DURATION = 20;
-var FREQUENCY = 10;
-var SAMPLING = 1;
+var DURATION, FREQUENCY, SAMPLING, MAX_FORCE, mass;
 
 var target = "1";
 
 var ctxPos, ctxVel, ctxForce, graphPos, graphVel, graphForce;
 
-var dsPos, dsTarget, dsDTarget, dsVel, dsForce, dsOutput;
+var simulationFrames, displayFrames, dsPos, dsTarget, dsDTarget, dsVel, dsForce, dsOutput;
 
 function PID(p, i, d) {
     this.p = p;
@@ -29,18 +28,15 @@ PID.prototype.push = function(error, dt) {
 };
 
 function derivative(fun) {
-    var h = 0.00000001;
     return function(x) {
-        return (fun(x + h) - fun(x)) / h;
+        return (fun(x + EPSILON) - fun(x)) / EPSILON;
     };
 }
 
 function simulate(args) {
     var pid = new PID(args.pid.p, args.pid.i, args.pid.d);
     const dt = 1 / args.frequency;
-    const frames = _.range(0, args.duration, dt).map(function(t) {
-        return Math.round(t * 100) / 100;
-    });
+    
     const dtarget = derivative(args.target);
 
     var x = 0;
@@ -53,26 +49,29 @@ function simulate(args) {
     var p_ = [];
     var t_ = [];
 
-    for (var i = 0; i <= frames.length; i++) {
-        var t = frames[i];
+    for (var i = 0; i <= simulationFrames.length; i++) {
+        var t = simulationFrames[i];
         const target = args.target(t);
         const deriv = dtarget(t);
         const raw = pid.push(target - x, dt) + args.pid.f * deriv;
 
         const force = Math.min(Math.max(raw, -args.forceLimit), args.forceLimit);  // Constrain the force
+        const acc = force / args.mass;
         v += force * dt;
         x += v * dt;
 
-        x_.push(x);
-        v_.push(v);
-        d_.push(deriv);
-        f_.push(force);
-        p_.push(raw);
-        t_.push(target);
+        if (i % SAMPLING == 0) {
+            x_.push(x);
+            v_.push(v);
+            d_.push(deriv);
+            f_.push(force);
+            p_.push(raw);
+            t_.push(target);
+        }
     }
 
     return {
-        time: frames, 
+        time: simulationFrames, 
         pos: x_, 
         vel: v_, 
         force: f_, 
@@ -82,7 +81,16 @@ function simulate(args) {
     };
 }
 
-function onSliderChange() {
+function copyInto(from, to) {
+    for (var i = from.length - 1; i >= 0; i--) {
+        to[i] = from[i];
+    }
+}
+
+function updateGraphOutput() {
+    var startTime = new Date().getMilliseconds();
+    target = $("#target-function").val();
+
     var pid = {
         p: $("#p-slider").val(),
         i: $("#i-slider").val(),
@@ -95,29 +103,19 @@ function onSliderChange() {
     $("#d-value").text(pid.d);
     $("#f-value").text(pid.f);
 
-    updateChart(pid);
-}
-
-function copyInto(from, to) {
-    for (var i = from.length - 1; i >= 0; i--) {
-        to[i] = from[i];
-    }
-}
-
-function updateChart(pidArgs) {
+    
     var simulationArgs = {
-        pid: pidArgs,
+        pid: pid,
         target: function(t) {
             return eval(target);
         },
-        duration: 20,
-        frequency: 10,
-        forceLimit: 10
+        mass: mass,
+        duration: DURATION,
+        frequency: FREQUENCY,
+        forceLimit: MAX_FORCE
     };
 
-    var startTime = new Date().getMilliseconds();
     var output = simulate(simulationArgs);
-    var completedIn = new Date().getMilliseconds() - startTime;
 
     copyInto(output.pos, dsPos);
     copyInto(output.vel, dsVel);
@@ -129,59 +127,21 @@ function updateChart(pidArgs) {
     graphPos.update();
     graphVel.update();
     graphForce.update();
+
+    var completedIn = new Date().getMilliseconds() - startTime;
+    $("#completed-in").text(completedIn);
+
 }
 
-function updateTargetFunction() {
-    target = $("#target-function").val();
-    onSliderChange();
-}
-
-$(function() {
-    $(".pid-slider").each(function() {
-        $(this)[0].oninput = onSliderChange;
-    });
-
-    $("#target-function").on("keydown", function(e) {
-        if (e.keyCode == 13) {
-            updateTargetFunction();
-        }
-    });
-
-    var frames = _.range(0, DURATION, 1 / FREQUENCY).map(function(t) {
-        return Math.round(t * 100) / 100;
-    });
-    dsPos = frames.slice();
-    dsTarget = frames.slice();
-    dsVel = frames.slice();
-    dsDTarget = frames.slice();
-    dsForce = frames.slice();
-    dsOutput = frames.slice();
+function initializeCharts() {
 
     ctxPos = $("#graphPos");
+    ctxVel = $("#graphVel");
+    ctxForce = $("#graphForce");
+
     graphPos = new Chart(ctxPos, {
         type: "line",
         cubicInterpolationMode: "monotone",
-        data: {
-            labels: frames,
-            datasets: [
-                {
-                    label: "Position",
-                    //backgroundColor: "rgb(255,0,0)",
-                    borderColor: "rgb(255,0,0)",
-                    fill: false,
-                    data: dsPos,
-                    pointRadius: 2
-                },
-                {
-                    label: "Target",
-                    //backgroundColor: "rgb(0,0,255)",
-                    borderColor: "rgb(0,0,255)",
-                    fill: false,
-                    data: dsTarget,
-                    pointRadius: 2
-                },
-            ]
-        },
         options: {
             responsive: true,
             hoverMode: "index",
@@ -223,30 +183,10 @@ $(function() {
         }
     });
     
-    ctxVel = $("#graphVel");
     graphVel = new Chart(ctxVel, {
         type: "line",
         cubicInterpolationMode: "monotone",
-        frames: frames,
-        data: {
-            labels: frames,
-            datasets: [
-                {
-                    label: "Velocity",
-                    borderColor: "rgb(0,128,0)",
-                    fill: false,
-                    data: dsVel,
-                    pointRadius: 2,
-                },
-                {
-                    label: "Target Derivative",
-                    borderColor: "rgb(0,128,255)",
-                    fill: false,
-                    data: dsDTarget,                 
-                    pointRadius: 2,
-                }
-            ]
-        },
+        frames: displayFrames,
         options: {
             responsive: true,
             hoverMode: "index",
@@ -288,29 +228,9 @@ $(function() {
         }
     });
 
-    ctxForce = $("#graphForce");
     graphForce = new Chart(ctxForce, {
         type: "line",
         cubicInterpolationMode: "monotone",
-        data: {
-            labels: frames,
-            datasets: [
-                {
-                    label: "Raw PID Output",
-                    fill: false,
-                    borderColor: "rgb(255, 255, 0)",
-                    data: dsOutput,                 
-                    pointRadius: 2
-                },
-                {
-                    label: "Constrained Force",
-                    borderColor: "rgb(255, 0, 255)",
-                    fill: false,
-                    data: dsForce,                 
-                    pointRadius: 2
-                }
-            ]
-        },
         options: {
             responsive: true,
             hoverMode: "index",
@@ -351,5 +271,106 @@ $(function() {
             }
         }
     }); 
-    onSliderChange();
+    
+}
+
+function updateSimulationParams() {
+
+    DURATION = parseFloat($("#sim-duration").val());
+    FREQUENCY = parseFloat($("#sim-freq").val());
+    SAMPLING = parseFloat($("#sim-sample").val());
+    MAX_FORCE = parseFloat($("#sim-force-limit").val());
+    mass = parseFloat($("#sim-mass").val());
+
+    simulationFrames = _.range(0, DURATION, 1 / FREQUENCY).map(function(t) {
+        return Math.round(t * 100) / 100;
+    });
+    displayFrames = simulationFrames.filter(function(x, i) {
+        return i % SAMPLING == 0;
+    });
+
+    dsPos = displayFrames.slice();
+    dsTarget = displayFrames.slice();
+    dsVel = displayFrames.slice();
+    dsDTarget = displayFrames.slice();
+    dsForce = displayFrames.slice();
+    dsOutput = displayFrames.slice();
+
+    ctxPos = $("#graphPos");
+
+    graphPos.data = {
+        labels: displayFrames,
+        datasets: [
+            {
+                label: "Position",
+                //backgroundColor: "rgb(255,0,0)",
+                borderColor: "rgb(255,0,0)",
+                fill: false,
+                data: dsPos,
+                pointRadius: 2
+            },
+            {
+                label: "Target",
+                //backgroundColor: "rgb(0,0,255)",
+                borderColor: "rgb(0,0,255)",
+                fill: false,
+                data: dsTarget,
+                pointRadius: 2
+            },
+        ]
+    };
+    graphVel.data = {
+        labels: displayFrames,
+        datasets: [
+            {
+                label: "Velocity",
+                borderColor: "rgb(0,128,0)",
+                fill: false,
+                data: dsVel,
+                pointRadius: 2,
+            },
+            {
+                label: "Target Derivative",
+                borderColor: "rgb(0,128,255)",
+                fill: false,
+                data: dsDTarget,                 
+                pointRadius: 2,
+            }
+        ]
+    };
+    graphForce.data = {
+        labels: displayFrames,
+        datasets: [
+            {
+                label: "Raw PID Output",
+                fill: false,
+                borderColor: "rgb(255, 255, 0)",
+                data: dsOutput,                 
+                pointRadius: 2
+            },
+            {
+                label: "Constrained Force",
+                borderColor: "rgb(255, 0, 255)",
+                fill: false,
+                data: dsForce,                 
+                pointRadius: 2
+            }
+        ]
+    };
+    updateGraphOutput();
+    
+}
+
+$(function() {
+    $(".pid-slider").each(function() {
+        $(this)[0].oninput = updateGraphOutput;
+    });
+
+    $("#target-function").on("keydown", function(e) {
+        if (e.keyCode == 13) {
+            updateGraphOutput();
+        }
+    });
+    initializeCharts();
+    updateSimulationParams();
 });
